3de35b6206d0939f0ce665388cf82b1e1569e527
KhalidUi <97871546+KhalidUi@users.noreply.github.com>
2022-01-24 16:53:29 +0530

    Updated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Transaction from './Transaction';
+
+
+
+function App() {
+  return (
+    <div >
+      <Transaction/>
+    </div>
+      );
+    }
+
+export default App;
